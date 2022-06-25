99fe2ddcd69e5eb9e00ef81a2c506f9b2174a6e9
zulker01 <zulker02@gmail.com>
2022-06-25 11:53:42 +0600

    done till miami univ

--- extractData.py
+++ extractData.py
@@ -1,14 +1,17 @@
 from openpyxl import load_workbook
- 
+import re
+
 wb = load_workbook("temp.xlsx")   # open an Excel file and return a workbook
-univName = "Indiana University-Purdue University" 
+univName = "Miami University" 
 courseCodeString = "CSCI"  
 if univName  in wb.sheetnames:
     print(univName+' exists')
+
 else:
     wb.create_sheet(univName)
 
 activesheet = wb[univName]
+
 if(activesheet["A1"]!="Course No"):
     #activesheet.append(("Course No","Course Name"))
     activesheet["A1"]="Course No"
@@ -19,29 +22,46 @@ courseCode=[]
 duplicateCount=0
 tempcode=""
 tempcode=""
+
 with open('temp.txt',encoding="utf8") as f:
     lines = f.readlines()
     for i in range(0, len(lines)):
         line = lines[i]   
-        if(courseCodeString in line[:6]):
+        if("CSE" in line[:5]):
                 
-                if("CSCI-N" in line[:7]):
+                #if("EN." in line[:4]):
+                ##hipposi = tmpstr.find(".")+7
+                tempcode = line[:8]
+                
+                tempName = line[8:]
+                if("(" in tempName[:2]):
+                    tempName = tempName[tempName.find(")")+1:]
+                #tempName = tempName[:tempName.find(".")]
+                #print("before tempcod "+tempcode)
+                
+                """
+                if("(" in tempName):
+                    
+                    tempName = tempName[tempName.find(")")+1:]
+                    #print("AFTER tempname "+tempName)
+                first_digit = re.search('\d', tempName)
+               # print(first_digit.start())
+                tempName = tempName[:first_digit.start()]
+                #elif("CSCI-C" in line[:7]):
                     tmpstr = line[5:]
                     hipposi = tmpstr.find("-")+5
                     tempcode = "CSCI-"+line[5:hipposi]
                     tempName = line[hipposi+1:]
-                elif("CSCI-C" in line[:7]):
-                    tmpstr = line[5:]
-                    hipposi = tmpstr.find("-")+5
-                    tempcode = "CSCI-"+line[5:hipposi]
-                    tempName = line[hipposi+1:]
-                else:
+                #else:
                     tempcode = line[:line.find("-")]
                     tempName = line[line.find("-")+1:]
+                    """
                 if(tempcode  in courseCode):
                     print("duplicate for : "+line+" total duplicate "+str(duplicateCount))
                     duplicateCount+=1
                     continue
+                if(tempName[0]==" "):
+                    tempName = tempName[1:]
                 courseCode.append(tempcode)
                 activesheet.append((tempcode,tempName))
 
